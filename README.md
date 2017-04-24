bc20489a740988933f7e105da8d053a92bcb061a
Steve Loughran <stevel@apache.org>
2017-04-24 23:29:19 +0100

    multi-user dynamic heckle working

--- README.md
+++ README.md
@@ -17,11 +17,27 @@ party members.
 There's a gemfile set up for ruby dependencies; `bundle install` will
 handle that
 
-### Config
+### Getting up and running
 
 1. Get a Twitter API credential set.
-1. copy `conf/example-secrets.rb` to `conf/secrets.rb`.
-1. 
+1. copy `conf/example-secrets.rb` to `conf/secrets.rb`. That is marked as gitignored.
+1. Start the bot: `ruby dissident.rb`
+
+
+### How to heckle
+
+For every user you wish to heckle
+
+1. Follow the target user from the account.
+1. Add a file `data/$user.txt`, where `$user` is the username. The example `data/conservatives.txt` does
+this for the conservative's twitter account.
+1. Add entries in the text file for every user: one per line. Keep the text short enough that the username + message is < 140 chars. You don't need to include the target name in the message.
+1. Twitter does throttle clients; there's a limit to how often you can heckle per minute. Focus, or create new accounts.
+
+You do not need to restart the bot to add/remove users, or to change the messages. The directory is scanned
+for a user, and their message file read, *for every tweet*. It's easier to do this than implement some kind
+of cache data structure.
+
 
 ## References
 
--- dissident.rb
+++ dissident.rb
@@ -9,57 +9,68 @@
 
 require 'twitter'
 
-
 class Heckles
+  
   # here are the heckles for a user
   
-  def init(file)
-    @last = -1
-    raw = IO.read(file)
-    raw.delete_if {|l| l.empty? or l.startswith("\#") }
+  def init(username)
+    @phrases = Array.new 
+    filename = "data/#{username}.txt"
+    if not File.file?(filename) 
+      puts "No data file #{filename}"
+      return false
+    end
+    
+    puts "Reading #{filename}"
+    File.open(filename).readlines.each do | line |
+      line.strip!
+      if not line.empty? and not line.start_with?("\#")
+        puts "#{line.length}: #{line}"
+        if line.length < 140
+          @phrases << line
+        else
+          puts "LINE too long"
+        end
+      end     
+    end
+    puts "Found #{@phrases.length} entries"
+    return true
+  end
   
+  def heckle
+    return @phrases.sample
+  end
+  
+  def empty?
+    return @phrases.length == 0
+  end
 end
 
 class Dissident
 
   def initialize
     config = eval(File.open('conf/secrets.rb') {|f| f.read })
-    @client = Twitter::REST::Client.new(config)
+    @rest = Twitter::REST::Client.new(config)
     @streaming = Twitter::Streaming::Client.new(config)
     @myname = "dissidentbot"
-    
-    # @phrases = [
-    #       'The president lost the popular vote by three million votes',
-    #       'Today, the amount of carbon dioxide in the atmosphere is higher than at any time in the last 650,000 years',
-    #       'Burning one gallon of gasoline puts nearly 20lbs of carbon dioxide into our atmosphere. #climate',
-    #       'Flipside of the atmosphere; ocean acidity has increased 30% since the industrial revolution. #climate',
-    #       'The pre-industrial concentration of carbon dioxide in the atmosphere was 280 ppm. As of December 2016, 404.93 ppm'
-    #     ]
-
-  @phrases = [
-    #    1        10        20        30        40        50         60        70         80         90         100       110       120
-        'No party with Boris Johnson in cabinet can accuse other parties of chaos. #dissent',
-        'Crush the Saboteurs! Especially remain voters in your own party! #dissent',
-        'Your hard Brexit plans mean you can\'t use the "chaos and economic disaster" threats this year. Please Upgrade. #dissent',
-        'What is it about Jeremy Corbyn that makes #AskTheresaMay scared to debate him? #dissent',
-        'The leave campaign didn\'t just lie, they broke the law? http://www.bbc.co.uk/news/uk-politics-39672956 #dissent',
-        'Why no manifesto promise of £350M/week for the NHS? #dissent',
-#        '',
-      ]
-
-
   end
   
 
-  def reply(tweet, message)
+  def reply(tweet)
     return unless tweet.in_reply_to_status_id.is_a?(Twitter::NullObject)
-    return if tweet.user.screen_name.eql?@myname
-    return if tweet.user.screen_name.eql?@myname and 
-    status = "@#{tweet.user.screen_name} #{message}"
-    puts "tweeting #{status}"
-    @client.update(status, in_reply_to_status_id: tweet.id)
+    username = tweet.user.screen_name
+    return if username.eql?@myname
+    heckles = Heckles.new()
+    heckles.init(username)
+    return if heckles.empty?
+    status = "@#{username} #{heckles.heckle}"
+    if status.length > 140
+      puts "Reply too long at #{status.length}: #{status}"
+    else
+      puts "tweeting #{status}"
+      @rest.update(status, in_reply_to_status_id: tweet.id)
+    end
   end
-
 
 # client.update("Dissent is a right; dissent is a duty")
 
@@ -71,7 +82,7 @@ class Dissident
       when Twitter::Tweet
         tweet = object
         puts "tweet: #{tweet.user.screen_name}: #{tweet.text} in reply to \"#{tweet.in_reply_to_user_id}\" "
-        reply(tweet, @phrases.sample)
+        reply(tweet)
       when Twitter::DirectMessage
         puts "It's a direct message!"
       when Twitter::Streaming::StallWarning
@@ -79,6 +90,7 @@ class Dissident
       end
     end
   end
+  
 
 end
 
